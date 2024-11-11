a0321702130321ec7d5067f137f5f16ca555ad63
moumou <1482283801@qq.com>
2024-11-11 20:15:16 +0800

    feat(custom): 第六次测试

---
+++ packages/ui/dist/entry.d.ts
@@ -0,0 +1,10 @@
+import type { App } from 'vue';
+import 'virtual:uno.css';
+export * from './avatar';
+export * from './button';
+export * from './link';
+declare const _default: {
+    install(app: App): void;
+    version: string;
+};
+export default _default;
--- packages/ui/src/entry.ts
+++ packages/ui/src/entry.ts
@@ -1,21 +1,21 @@
-// import SButton from './button/Button'
-// import SFCButton from './SFCButton.vue'
-// import TSXButton from './TSXButton'
-
-// 导出单独组件
-// export { SButton, SFCButton, TSXButton }
-
-// 编写一个插件，实现一个 install 方法
-// export default {
-//   install(app: App): void {
-//     app.component(SButton.name, SButton)
-//     app.component(SFCButton.name, SFCButton)
-//     app.component(TSXButton.name, TSXButton)
-//   },
-// }
-
 // import type { Plugin } from 'vue'
-// import { version } from '../../../package.json'
+// import { version } from '../package.json'
+// import plugins from './plugins'
+// import 'virtual:uno.css'
+
+// // 导出组件
+// export * from './button'
+// export * from './link'
+
+import type { App } from 'vue' // 导入 App 类型
+// // 导出插件
+// export default {
+//   install(app: { use: (arg0: Plugin) => void }) {
+//     plugins.forEach(c => app.use(c))
+//   },
+//   version,
+// }
+import { version } from '../package.json'
 import plugins from './plugins'
 import 'virtual:uno.css'
 
@@ -23,11 +23,10 @@ export * from './avatar'
 // 导出组件
 export * from './button'
 export * from './link'
-
 // 导出插件
 export default {
-  install(app: { use: (arg0: Plugin) => void }) {
+  install(app: App) {
     plugins.forEach(c => app.use(c))
   },
-  // version,
+  version,
 }
